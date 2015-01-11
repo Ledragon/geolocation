c3e3622f830243ce331af4ef5e3fec4b12b08300
Ledragon <hugues.stefanski@gmail.com>
2015-01-11 20:42:23 +0100

    Coloring according to continent

---
+++ Geolocalisation/scripts/topojson.d.ts
@@ -0,0 +1,16 @@
+﻿// Type definitions for TopoJSON
+// Project: https://github.com/mbostock/topojson
+// Definitions by: Hugues Stefanski <https://github.com/Ledragon/>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped 
+declare module TopoJSON {
+    export interface TopoJSON {
+        feature(topology, object);
+        merge(topology, objects);
+        mesh(topology, object, filter?);
+        meshArcs(topology, objects?, filter?);
+        neighbors(objects);
+    }
+
+}
+
+declare var topojson: TopoJSON.TopoJSON;
