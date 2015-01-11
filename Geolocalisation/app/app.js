e9e49eac3119066db70762cb0720f47a76c84b80
Ledragon <hugues.stefanski@gmail.com>
2015-01-11 17:53:49 +0100

    UK map displayed

--- Geolocalisation/app/app.js
+++ Geolocalisation/app/app.js
@@ -19,7 +19,7 @@
                     //console.log(uk);
                     var subunits = topojson.feature(uk, uk.objects.subunits);
                     var mercatorProjection = d3.geo.mercator()
-                    .scale(500)
+                    .scale(100)
                     .translate([2 * width / 3, 2 / 3 * height]);
 
                     var albersProjection = d3.geo.albers()
@@ -28,12 +28,55 @@
                     .parallels([50, 60])
                     .scale(3000)
                     .translate([width / 2, height / 2]);
-
-
                     var path = d3.geo.path().projection(albersProjection);
-                    svg.append('path')
-                    .datum(subunits)
+
+                    svg.selectAll('.subunit')
+                    .data(subunits.features)
+                    .enter()
+                    .append('path')
+                    .attr('class', function (d) { return 'subunit ' + d.id; })
                     .attr('d', path);
+                    //.style({
+                    //    fill: function (d, i) {
+                    //        var color = '#ccc';
+                    //        if (d.continent === 'Europe') {
+                    //            color = '#39c';
+                    //        }
+                    //        return color;
+                    //    }
+
+                    //});
+
+                    svg.append("path")
+    .datum(topojson.mesh(uk, uk.objects.subunits, function (a, b) { return a !== b && a.id !== "IRL"; }))
+    .attr("d", path)
+    .attr("class", "subunit-boundary");
+
+                    svg.append("path")
+    .datum(topojson.mesh(uk, uk.objects.subunits, function (a, b) { return a === b && a.id === "IRL"; }))
+    .attr("d", path)
+    .attr("class", "subunit-boundary IRL");
+
+                    svg.append("path")
+    .datum(topojson.feature(uk, uk.objects.places))
+    .attr("d", path)
+    .attr("class", "place");
+
+                    svg.selectAll(".place-label")
+    .data(topojson.feature(uk, uk.objects.places).features)
+  .enter().append("text")
+    .attr("class", "place-label")
+    .attr("transform", function (d) { return "translate(" + albersProjection(d.geometry.coordinates) + ")"; })
+    .attr("dy", ".35em")
+    .text(function (d) { return d.properties.name; });
+
+                    svg.selectAll(".place-label")
+    .attr("x", function (d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+    .style("text-anchor", function (d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; });
+
+                    //svg.append('path')
+                    //.datum(subunits)
+                    //.attr('d', path);
                 }
             });
         }
